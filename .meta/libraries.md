cd0b9ad2eb409f3c92aad76323057b56d5cd1b11
velia-vito <91265151+velia-vito@users.noreply.github.com>
2025-01-30 20:52:52 +0530

    Package Setup

---
+++ .meta/libraries.md
@@ -0,0 +1,11 @@
+Potential Libraries
+
+ID3 Libraries
+    - id3_codec
+    - eztags
+
+Youtube
+    - youtube_explode_dart
+
+FFmpeg
+    - ffmpeg_wasm
---
+++ example/spotdl_dart_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:spotdl_dart_core/spotdl_dart_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
